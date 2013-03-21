b37f323d7620eae04dd4876129379258948b8645
Matt Mueller <mattmuelle@gmail.com>
2013-03-20 19:20:44 -0700

    cleanup

--- test/lexer.js
+++ test/lexer.js
@@ -5,18 +5,12 @@
 var read = require('fs').readFileSync,
     parser = require('../lib/parser');
     compile = require('../lib/compiler'),
-    util = require('util');
+    beautify = require('js-beautify').html;
 
 var note = read('./cases/note.txt', 'utf8');
 
 compile(note, function(err, str) {
   if(err) throw err;
-
-  var tidy = require('htmltidy').tidy;
-  console.log(str);
-
-  tidy(str, function(err, out) {
-    if(err) console.log(err);
-    console.log(out);
-  });
+  var out = beautify(str, { indent_size : 2 })
+  console.log(out);
 });
